5215831c80b689e5dce6d9e6253e002d974eac6f
Edwin Wong <edwinfwong@gmail.com>
2015-01-23 23:52:31 -0800

    Started working on basic ngmodel

--- demos/angular-multiselect.js
+++ demos/angular-multiselect.js
@@ -1,13 +1,34 @@
 (function(angular){
     'use strict';
-    angular.module('ngDirectives', [])
+    angular.module('ngMultiselect', [])
     .directive('jqueryUiMultiselect', function () {
-
+		var template =  '<div><select multiple="multiple" ng-model="ngModel" ng-options="item.text for item in optionList track by item.value"></select></div>';
         return {
             restrict: 'EA',
-            link: function ($scope, $element, $attributes) {
-                $element.multiselect();
-            }
+			require: 'ngModel',
+			replace:true,
+			scope:{
+				//ngModel: '=',
+				optionList: '='
+			},
+            link: function ($scope, $element, $attributes, ngModel) {
+				
+				ngModel.$render = function(){
+					console.log('rendering');
+					
+				}
+				
+				$scope.$watch($attributes.ngModel, function(modelValue){
+					ngModel.$setViewValue(modelValue);
+					$element.multiselect('refresh');	
+					alert('hello');
+				});
+				
+				
+				
+				$element.multiselect();	
+            },
+			template: template
         }
     });
 })(angular);
